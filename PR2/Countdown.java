aa891c96732183594413b7ddbb2e48374c68400c
Busheng <bulou@eng.ucsd.edu>
2015-04-16 12:53:05 -0700

    1

--- PR2/Countdown.java
+++ PR2/Countdown.java
@@ -14,23 +14,24 @@ public class Countdown extends WindowController {
   
   private final static int CDIAMETER = 10;
   
-  private static int width=800, height=800, countdown =26;
+  private static int width, height, countdown;
   private final static int second = 1000;
   public void begin() {
-    int cir_x = (width - CDIAMETER)/2, cir_y = (height - CDIAMETER)/2;
+    int Width = canvas.getWidth(), Height = canvas.getHeight();
+    int cir_x = (Width - CDIAMETER)/2, cir_y = (Height - CDIAMETER)/2;
     new FilledOval(cir_x, cir_y, CDIAMETER, CDIAMETER, canvas).setColor(Color.blue);
-    int length = width > height? width:height;
+    int length = Width > Height? Height:Width;
     double angle = 0;
     AngLine secondhand;
-    
+
     while(countdown-- != 0) {
       angle = Math.PI * (countdown + 15) / 30; 
-      secondhand = new AngLine(width/2, height/2, length/2, angle, canvas);
+      secondhand = new AngLine(Width/2, Height/2, length/2, angle, canvas);
       try { TimeUnit.MILLISECONDS.sleep(second);}
       catch (InterruptedException e){};
       secondhand.hide();
     }
-    secondhand = new AngLine(width/2, height/2, length/2, angle, canvas);
+    secondhand = new AngLine(Width/2, Height/2, length/2, Math.PI/2, canvas);
   }
   
   public void onMouseClick(Location point) {
@@ -39,17 +40,17 @@ public class Countdown extends WindowController {
   
   public static void main(String[] args) {
     Scanner scnr = new Scanner(System.in);
-       /*
     System.out.print("Enter clock width in pixels : ");
     width = scnr.nextInt();
     System.out.print("Enter clock height in pixels : ");
     height = scnr.nextInt();
     System.out.print("Enter countdown in seconds : ");
     countdown = scnr.nextInt();
-    */
+    if ((width <(CDIAMETER + 20)) || (height < (CDIAMETER + 20)) || (countdown < 0)) {
+      System.out.println("BAD INPUT");
+      System.exit(1);
+    }
     new Countdown().startController(width,height);
   }
-
-
 
 }
