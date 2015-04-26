5cb5ee4b20b2f4cac90885af6aaa05aea3257d8b
Busheng <bulou@eng.ucsd.edu>
2015-04-25 23:29:48 -0700

    mod

--- PR4/AsciiArt.java
+++ PR4/AsciiArt.java
@@ -11,7 +11,9 @@ import java.util.ArrayList;
 public class AsciiArt {
    
   public static AsciiGrid grid11;
-  
+  private static final boolean TRIANGLE = true;
+  private static final boolean RECTANGLE = false;
+
   public static void main(String[] args) {
     ArrayList<AsciiShape> shapelist = new ArrayList<AsciiShape>();
     Scanner scnr = new Scanner(System.in);
@@ -19,21 +21,39 @@ public class AsciiArt {
     System.out.print(prompt);
 
     while (scnr.hasNext()) {
+      boolean first_int = false;
       boolean good_input = false;
       String inputs = scnr.nextLine();
       String[] input = inputs.split("\\s+");
       String command = input[0].toLowerCase();
-      int[] arguments = getArgs(input);
-      int len = arguments.length;
+      if (input.length > 1 && input[1].matches("\\d+$")) 
+        first_int = true;
+      int[] argus = getArgs(first_int,input);
+      int len = argus.length;
       switch (command) {
         case "triangle":
+	  if (!first_int) {
+	    char symbol = input[1].charAt(0);
+	    shapelist.add(new AsciiShape(TRIANGLE,argus[0],argus[1], symbol));
+	  } else {
+	    shapelist.add(new AsciiShape(TRIANGLE,argus[0],argus[1]));
+	  }
 	  break;
         case "rectangle":
+	  if (!first_int) {
+	    char symbol = input[1].charAt(0);
+	    shapelist.add(new AsciiShape(RECTANGLE,argus[0],argus[1], symbol));
+	  } else {
+	    shapelist.add(new AsciiShape(RECTANGLE,argus[0],argus[1]));
+	  }
 	  break;
         case "list":
+	  for (AsciiShape shape : shapelist) {
+	    System.out.println(shape.toString());
+	  }
 	  break;
         case "new":
-	  good_input = grid_new(arguments);
+	  good_input = grid_new(argus);
 	  break;
         case "print":
 	  if (len == 0) {
@@ -67,11 +87,22 @@ public class AsciiArt {
    * @param inputs array of input strings 
    * @return a int array of arguments
   */
-  public static int[] getArgs(String[] inputs) {
-    int[] rel = new int[inputs.length - 1];
-    int i = 1;
+  public static int[] getArgs(boolean first_int, String[] inputs) {
+    if (inputs.length == 1) {
+      return new int[0];
+    }
+    int [] rel;
+    int shift;
+    if (first_int) {
+      rel = new int[inputs.length - 1];
+      shift = 1;
+    } else {
+      rel = new int[inputs.length - 2];
+      shift = 2;
+    }
+    int i = shift;
     while (i != inputs.length) {
-      rel[i-1] = Integer.parseInt(inputs[i]);
+      rel[i-shift] = Integer.parseInt(inputs[i]);
       i++;
     }
     return rel;
