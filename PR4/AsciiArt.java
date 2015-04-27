a7173ef4cf63290e3858412769b62aad07e1d200
Busheng <bulou@eng.ucsd.edu>
2015-04-26 18:15:17 -0700

    final

--- PR4/AsciiArt.java
+++ PR4/AsciiArt.java
@@ -11,8 +11,9 @@ import java.util.ArrayList;
 public class AsciiArt {
    
   public static AsciiGrid grid11;
-  private static final boolean TRIANGLE = true;
-  private static final boolean RECTANGLE = false;
+  public static boolean grid_exist = false;
+  private static final boolean TRIANGLE = true, RECTANGLE = false;
+  private static final boolean PLACE = true, CLEAR = false;
 
   public static void main(String[] args) {
     ArrayList<AsciiShape> shapelist = new ArrayList<AsciiShape>();
@@ -21,14 +22,16 @@ public class AsciiArt {
     System.out.print(prompt);
 
     while (scnr.hasNext()) {
-      boolean first_int = false;
-      boolean good_input = false;
+      boolean first_int = false, good_input = false;
       String inputs = scnr.nextLine();
       String[] input = inputs.split("\\s+");
       String command = input[0].toLowerCase();
-      if (input.length > 1 && input[1].matches("\\d+$")) 
-        first_int = true;
-      int[] argus = getArgs(first_int,input);
+      int[] argus = new int[0];
+      if ((input.length > 1 && input[1].matches("\\d+$"))
+                                    || input.length == 1) { 
+        argus = getArgs(input);
+	first_int = true;
+      }
       int len = argus.length;
       switch (command) {
         case "triangle":
@@ -38,24 +41,22 @@ public class AsciiArt {
 	  good_input = shape_new (RECTANGLE, shapelist, input);
 	  break;
         case "list":
-	  good_input = grid_list(argus, shapelist);
+	  good_input = grid_list(first_int, argus, shapelist);
 	  break;
         case "new":
-	  good_input = grid_new(argus);
+	  good_input = grid_new(first_int, argus);
 	  break;
         case "print":
-	  good_input = grid_print(argus);
+	  good_input = grid_print(first_int, argus);
 	  break;
         case "place":
-	  good_input = grid11.placeShape(shapelist.get(argus[0]).getShape(),argus[1],argus[2]);
+          good_input = grid_place_clear(first_int, PLACE, shapelist, argus);	
 	  break;
         case "clear":
-	  good_input = grid11.clearShape(shapelist.get(argus[0]).getShape(),argus[1],argus[2]);
+          good_input = grid_place_clear(first_int, CLEAR, shapelist, argus);	
 	  break;
         case "set":
-	  char symbol = input[1].charAt(0);
-	  if (len == 0)
-	  shapelist.get(argus[0]).setSymbol(symbol);
+	  good_input = grid_set(shapelist, input);
 	  break;
         case "exit":
           System.out.println(ok);
@@ -77,26 +78,16 @@ public class AsciiArt {
    * @param inputs array of input strings 
    * @return a int array of arguments     
   */
-  public static int[] getArgs(boolean first_int, String[] inputs) {
-    if (inputs.length == 1) {
-      return new int[0];
-    }
-    int [] rel;
-    int shift;
-    if (first_int) {
-      rel = new int[inputs.length - 1];
-      shift = 1;
-    } else {
-      rel = new int[inputs.length - 2];
-      shift = 2;
-    }
-    int i = shift;
+  public static int[] getArgs(String[] inputs) {
+    int[] rel = new int[inputs.length - 1];
+    int i = 1;
     while (i != inputs.length) {
-      rel[i-shift] = Integer.parseInt(inputs[i]);
+      rel[i-1] = Integer.parseInt(inputs[i]);
       i++;
     }
     return rel;
   }
+
   public static boolean shape_new (boolean triangle,
     ArrayList<AsciiShape> shapelist, String[] inputs) {
     if (inputs.length == 3) {
@@ -126,27 +117,29 @@ public class AsciiArt {
    * @param args input arguments
    * @return true when a object is created
   */
-  public static boolean grid_new (int[] args) {
+  public static boolean grid_new (boolean fir_int, int[] args) {
+    if (!fir_int) return false;
     if (args.length == 0) {
       grid11 = new AsciiGrid();
     } else if (args.length == 2) {
       grid11 = new AsciiGrid(args[0], args[1]);
     } else {
       return false;
-    }   
+    }
+    grid_exist = true;
     return true;
   }
 
-  public static boolean grid_print(int[] args) {
-    if (args.length != 0)
+  public static boolean grid_print(boolean fir_int, int[] args) {
+    if (args.length != 0 || !fir_int || !grid_exist)
       return false;
     System.out.println(grid11.toString());
     return true;
   }
   
-  public static boolean grid_list(int[] args, 
-             ArrayList<AsciiShape> shapelist) {
-    if (args.length != 0)
+  public static boolean grid_list(boolean fir_int, int[] args, 
+                              ArrayList<AsciiShape> shapelist) {
+    if (args.length != 0 || !fir_int)
       return false;
     int i = 0;
     for (AsciiShape shape : shapelist) {
@@ -154,5 +147,29 @@ public class AsciiArt {
       System.out.println(shape.toString());
     }
     return true;
+  }
+
+  public static boolean grid_place_clear (boolean fir_int, boolean place,
+                             ArrayList<AsciiShape> shapelist, int[] args) {
+    int nele = shapelist.size();
+    if (!fir_int || args.length !=3 || args[0] >= nele || !grid_exist) {
+      return false;
+    }
+    if (place) {
+      return grid11.placeShape(shapelist.get(args[0]).getShape(),args[1],args[2]);
+    } else {
+      return grid11.clearShape(shapelist.get(args[0]).getShape(),args[1],args[2]);
+    }
+  }
+
+  public static boolean grid_set(ArrayList<AsciiShape> shapelist,
+                                                 String[] inputs) {
+    int index = Integer.parseInt(inputs[2]);
+    if (inputs.length == 3 && index < shapelist.size()) {
+      char symbol = inputs[1].charAt(0);
+      shapelist.get(index).setSymbol(symbol);
+      return true;
+    } 
+    return false;
   }
 }
